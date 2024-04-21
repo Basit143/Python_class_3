72a163473a0012f659a1533276d00886c5fceda5
Abdul Basit <bbasitkdfbgvb@gmail.com>
2024-04-21 22:22:20 +0500

    Python_Class_03

---
+++ class3.py
@@ -0,0 +1,26 @@
+#hello world
+
+'''
+This is a multi-line
+comment or docstring
+'''
+print("Hello, World!")
+
+a : int = 7
+b : int = 2
+
+print(a + b) # addiction
+print(a - b) # subtraction
+print(a * b) # Multiplication
+print(a / b) # division
+print(a % b) # Modulus
+print(a ** b) # Exponentiation
+print(a // b) # Floor division
+
+
+
+
+names : list[str] = ["Abdul Basit" , "Abdul Dayyan" , "Qasim" , "Ahmad"]
+uinput : str = input("Enter your name")
+
+uinput not in names
